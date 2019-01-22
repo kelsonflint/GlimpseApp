8828470c7dd9ce7b293dbdb9ba61bf6829e01af8
Kelson Flint <kelson.flint@gmail.com>
2019-01-21 21:01:31 -0800

    Initial Commit

---
+++ LocalGlimpse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalGlimpse
+//
+//  Created by Kelson Flint on 1/21/19.
+//  Copyright © 2019 Kelson Flint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
